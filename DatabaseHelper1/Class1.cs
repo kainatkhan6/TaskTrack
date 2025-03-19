35d50eac53826fcbb5d220b382cef495213a6848
kainatkhan6 <kainatkhankk00@gmail.com>
2025-03-19 21:55:18 +0500

    Added DatabaseHelper1 and TaskManager1

---
+++ DatabaseHelper1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DatabaseHelper1
+{
+    public class Class1
+    {
+        private readonly string connectionString = "server=localhost;database=todo_db;uid=root;pwd=;";
+
+        public bool ExecuteQuery(string query, Dictionary<string, object> parameters)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    foreach (var param in parameters)
+                    {
+                        cmd.Parameters.AddWithValue(param.Key, param.Value);
+                    }
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public DataTable GetData(string query)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TaskManager1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseHelper1;
+
+namespace TaskManager1
+{
+    public class Class1
+    {
+        DatabaseHelper1.Class1 dbHelper1 = new DatabaseHelper1.Class1(); // Data Layer Instance
+
+        public bool AddTask(string taskName, string status)
+        {
+            string query = "INSERT INTO tasks (task_name, status) VALUES (@task_name, @status)";
+            var parameters = new Dictionary<string, object>
+            {
+                { "@task_name", taskName },
+                { "@status", status }
+            };
+            return dbHelper1.ExecuteQuery(query, parameters);  // ✅ Calls Data Layer
+        }
+
+        public DataTable GetTasks()
+        {
+            string query = "SELECT id, task_name, status FROM tasks";
+            return dbHelper1.GetData(query);  // ✅ Calls Data Layer
+        }
+    }
+}
